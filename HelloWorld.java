2eda7d9f3c3e361955f8af51bbe1fb9ef0efa934
Lucas Corrêa Agostinho <lucascagostinho@gmail.com>
2023-10-18 00:22:29 -0300

    Aprendendo sobre estrutura de classe

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+ * HelloWorld = Nome da minha classe
+ * public = Tipo de acesso da classe
+ * class = tipo da classe
+ */
+public class HelloWorld {
+
+    //conteúdo deverá sser inserido aqui dentro
+
+}
+//fora do escopo da classe
