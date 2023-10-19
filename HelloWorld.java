244bc7c177688e3e73dd62af0e4a74b78050306f
Lucas Corrêa Agostinho <lucascagostinho@gmail.com>
2023-10-19 00:42:51 -0300

    Aprendendo sobre tipo de dados e o metodo main

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,21 @@
  */
 public class HelloWorld {
 
-    //conteúdo deverá sser inserido aqui dentro
+    //conteúdo deverá ser inserido aqui dentro
+    public static void main(String[] args) {
+        /*
+         * Números(Valores) (int, double, float, long)
+         * Textos (String)
+         * Booleanos (boolean)
+         */
+        int dadoDoTipoInt = 10;
+        double dadoDoTipoDouble = 3.14;
+        float dadoDoTipoFloat = 3.14f;
+        long dadoDoTipoLong = 19619816516L;
+        String dadoDoTipoString = "Colocar o meu texto";
+        boolean dadoDoTipoBooleanTrue = true;
+        boolean dadoDoTipoBooleanFalse = false;
+    }
 
 }
 //fora do escopo da classe
