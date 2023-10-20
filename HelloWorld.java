01fdd998a2bf16adcb03a96404ba9b808663cd45
Lucas Corrêa Agostinho <lucascagostinho@gmail.com>
2023-10-20 01:02:12 -0300

    Aprendendo sobre esstrutura de decisao e repeticao

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /*
  * HelloWorld = Nome da minha classe
  * public = Tipo de acesso da classe
@@ -19,7 +21,41 @@ public class HelloWorld {
         String dadoDoTipoString = "Colocar o meu texto";
         boolean dadoDoTipoBooleanTrue = true;
         boolean dadoDoTipoBooleanFalse = false;
-    }
 
+        //if - else
+        if (dadoDoTipoInt == 10) {
+            //sysout
+            System.out.println("Entrou no if");
+        } else if (dadoDoTipoInt == 12) {
+            System.out.println("Entrou no if do 12");
+        } else {
+            System.out.println("Entrou no else");
+        }
+
+        if (dadoDoTipoInt == 10) {
+            System.out.println("Entrou no if do 10");
+        }
+        if (dadoDoTipoInt < 12) {
+            System.out.println("entrou no id do 12");
+        } else {
+            System.out.println("Entrou no else");
+        }
+
+        //while (Enquanto algo for verdadeiro, faça algo)
+        int valorInicial = 0;
+        while (valorInicial < 3) {
+            System.out.println("O valor inicial é menor que três");
+            System.out.println(valorInicial);
+            valorInicial++;
+        }
+        System.out.println("saiu do while");
+
+        //for
+        System.out.println("Iniciando o for");
+        for (int i = 0; i < 4; i++ ) {
+            System.out.println("O valor do i é: " + i);
+        }
+        System.out.println("Finalizando o for");
+    }
 }
 //fora do escopo da classe
